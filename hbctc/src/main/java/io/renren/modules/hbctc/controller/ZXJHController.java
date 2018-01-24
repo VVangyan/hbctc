5fd3ce2088b6e7d837f267422e77b99b68b4e1b7
yan <yan>
2018-01-24 15:08:40 +0800

    1.初始提交

---
+++ hbctc/src/main/java/io/renren/modules/hbctc/controller/ZXJHController.java
@@ -0,0 +1,16 @@
+package io.renren.modules.hbctc.controller;
+
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Description:执行计划controller
+ * @date: 2018-01-24
+ * @author: yan
+ */
+@Controller
+public class ZXJHController {
+}
---
+++ hbctc/src/main/resources/static/js/hbctc/jxsq/jxsq.js
@@ -0,0 +1,20 @@
+$(function () {
+    // alert(1)
+
+    function addzxjh() {
+        var btn=$("#btn_add_zxjh")
+        btn.on("click",function () {
+            $("#add_zxjh_Modal").modal({
+                backdrop:"static"
+            });
+
+
+
+
+
+
+        })
+    }
+    addzxjh()
+
+})
